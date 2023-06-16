b5a12b46a23db0f38c9a2367f33e78d1f0401fa6
Fran Ortega <ortegfran@gmail.com>
2023-06-16 16:35:57 +0200

    Creación de componentes Pokemon y Movimiento

--- pokedexFront/src/app/app.module.ts
+++ pokedexFront/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonComponent } from './components/pokemon/pokemon.component';
+import { MovimientoComponent } from './components/movimiento/movimiento.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonComponent,
+    MovimientoComponent
   ],
   imports: [
     BrowserModule,

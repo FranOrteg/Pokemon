e72c0e3ab150920f3f506ebf4c0bd223351b96c1
Fran Ortega <ortegfran@gmail.com>
2023-06-16 16:42:07 +0200

    Implementación de rutas y comienzo Parte 1 :Crear un método que, al pasarle un Pokémon, el movimiento escogido del mismo y su rival devuelva el cálculo del daño.

--- pokedexFront/src/app/app-routing.module.ts
+++ pokedexFront/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokemonComponent } from './components/pokemon/pokemon.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/pokemon' },
+  { path: 'pokemon', component: PokemonComponent },
+  { path: '**', redirectTo: '/pokemon' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

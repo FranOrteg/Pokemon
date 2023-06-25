41fc2d672f86cf319ac13f9c27f06d1f188d384e
Fran Ortega <ortegfran@gmail.com>
2023-06-25 21:17:37 +0200

    Arreglo Select Header y Validadores Formulario

--- pokedexFront/src/app/components/header/header.component.ts
+++ pokedexFront/src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-header',
@@ -9,21 +10,27 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent {
 
-  searchForm: FormGroup
-  @Output() nameEmitted: EventEmitter<string>
-  @Output() typeEmitted: EventEmitter<string>
-  @Output() AllEmitted: EventEmitter<void>
-  @Output() FormEmitted: EventEmitter<string>
-  @Input() pokemons: any[]
+  createForm: FormGroup
+  @Output() nameEmitted: EventEmitter<string>;
+  @Output() typeEmitted: EventEmitter<string>;
+  @Output() AllEmitted: EventEmitter<void>;
+  @Output() FormEmitted: EventEmitter<string>;
+  @Input() pokemons: any[];
+  selectedType: string = '';
+  arrPokemons: any[] = [];
+  uniqueTypes: string[] = [];
 
 
-  constructor(private router: Router) {
+  constructor(
+    private router: Router,
+    private pokemonService: PokemonService
+  ) {
     this.pokemons = []
     this.FormEmitted = new EventEmitter
     this.nameEmitted = new EventEmitter
     this.typeEmitted = new EventEmitter
     this.AllEmitted = new EventEmitter
-    this.searchForm = new FormGroup({
+    this.createForm = new FormGroup({
       nombre: new FormControl('',
         [
           Validators.required
@@ -67,43 +74,61 @@ export class HeaderComponent {
     }, []);
   }
 
+  // Emitimos el nombre del pokemon al componente padre
   getData(): void {
-    console.log(this.searchForm.value);
-    this.nameEmitted.emit(this.searchForm.value.nombre);
-    this.searchForm.reset()
+    console.log(this.createForm.value);
+    this.nameEmitted.emit(this.createForm.value.nombre);
+    this.createForm.reset()
   }
 
+  // Emitimos el tipo del pokemon al componente padre
   getType(): void {
-    const type = this.searchForm.value.tipo;
+    const type = this.createForm.value.tipo;
     if (type) {
+      this.selectedType = type;
       this.typeEmitted.emit(type);
-      this.searchForm.reset()
     }
+    this.createForm.reset();
   }
 
+  // Emitimos todos los pokemons al componente padre
   getAll(): void {
     this.AllEmitted.emit();
-    this.searchForm.reset();
+    this.createForm.reset();
   }
 
+  // Emitimos el formulario al componente padre
   onSubmit(): void {
-    console.log(this.searchForm.value)
-    this.FormEmitted.emit(this.searchForm.value);
-    this.searchForm.reset();
+    console.log(this.createForm.value)
+    this.FormEmitted.emit(this.createForm.value);
+    this.createForm.reset();
     this.router.navigate(['/pokedex'])
 
   }
 
-  getUniqueTypes(pokemons: any[]): string[] {
+  // Obtenemos todos los tipos de pokemon para el select
+  async ngOnInit(): Promise<void> {
+    this.arrPokemons = await this.pokemonService.GetAllPokemons();
     const uniqueTypes: string[] = [];
-    for (const pokemon of pokemons) {
-      if (!uniqueTypes.includes(pokemon.tipo)) {
-        uniqueTypes.push(pokemon.tipo);
+    for (const poke of this.arrPokemons) {
+      if (poke.tipo && poke.tipo.length > 1) {
+        const types = poke.tipo.split(',');
+        for (const type of types) {
+          if (!uniqueTypes.includes(type.trim())) {
+            uniqueTypes.push(type.trim());
+          }
+        }
+      } else if (poke.tipo) {
+        if (!uniqueTypes.includes(poke.tipo.trim())) {
+          uniqueTypes.push(poke.tipo.trim());
+        }
       }
     }
-    return uniqueTypes;
+    this.uniqueTypes = uniqueTypes;
   }
 
-
-
+  // Validamos los campos del formulario
+  checkError(control: string, validator: string) {
+    return this.createForm.get(control)?.hasError(validator) && this.createForm.get(control)?.touched;
+  }
 }
